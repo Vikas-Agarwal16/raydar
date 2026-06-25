28fdef1aedb37f1d236efcff44e07f6b1721ee7f
Vikas Agarwal <vikasagarwal.sma@gmail.com>
2026-06-26 00:47:06 +0530

    Remove temporary email test script

--- test-email.js
+++
@@ -1,9 +0,0 @@
-import { sendDigestEmail } from "./lib/email.js";
-
-const result = await sendDigestEmail(
-  "your_own_email@gmail.com", // send to yourself
-  "Raydar test email",
-  "<h1>If you see this, SMTP works</h1>"
-);
-
-console.log(result);
